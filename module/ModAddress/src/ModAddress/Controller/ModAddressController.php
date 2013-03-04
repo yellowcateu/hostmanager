33d10522134035bc1ed84792a594713a43664d31
alterone <tkowalski@.MSWS.LOCAL>
2013-03-04 09:12:09 +0100

    exp ep 2

--- module/ModAddress/src/ModAddress/Controller/ModAddressController.php
+++ module/ModAddress/src/ModAddress/Controller/ModAddressController.php
@@ -6,8 +6,24 @@ use Zend\View\Model\ViewModel;
 
 class ModAddressController extends AbstractActionController
 {
+    
+    protected $modaddressTable;
+     
+    public function getModAddressTable()
+    {
+        if (!$this->modaddressTable) {
+            $sm = $this->getServiceLocator();
+            $this->modaddressTable = $sm->get('ModAddress\Model\ModAddressTable');
+        }
+        return $this->modaddressTable;
+    }
+        
+        
     public function indexAction()
     {
+        return new ViewModel(array(
+            'addresses' => $this->getModAddressTable()->fetchAll(),
+        ));
     }
 
     public function addAction()
